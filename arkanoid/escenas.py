fec3f09ed61b391724c206411b4f6381336931a7
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-09-02 22:44:19 +0200

    Portada ma o meno

--- arkanoid/escenas.py
+++ arkanoid/escenas.py
@@ -1,11 +1,11 @@
 import pygame as pg
-from . import FPS
+from . import FPS, ANCHO
 
 class Escena():
     def __init__(self, pantalla):
         self.pantalla = pantalla
         self.reloj = pg.time.Clock()
-
+    
     def bucle_principal(self):
         pass
     
@@ -13,22 +13,35 @@ class Portada(Escena):
     def __init__(self, pantalla):
         super().__init__(pantalla)
         self.logo = pg.image.load("resources/images/arkanoid_name.png")
+        fuente = pg.font.Font("resources/fonts/CabinSketch-Bold.ttf", 80)
+        self.textito = fuente.render("Pulsa <SPC> para comenzar", True, (0,0,0))
+        self.anchoTexto = self.textito.get_width()
+
 
     def bucle_principal(self):
+        print("soy portada")
         game_over = False
         while not game_over:
             for evento in pg.event.get():
                 if evento.type == pg.QUIT:
-                    game_over = True
+                    exit()
+
+                if evento.type == pg.KEYDOWN:
+                    if evento.key == pg.K_SPACE:
+                        game_over = True
 
             self.pantalla.fill((80, 80, 255))
             self.pantalla.blit(self.logo, (140, 140))
-
+            self.pantalla.blit(self.textito, ((ANCHO - self.anchoTexto) // 2, 340))
             pg.display.flip()
+        
 
 
 class Partida(Escena):
-    pass
+    def bucle_principal(self):
+        print("soy partida")
 
 class Records(Escena):
-    pass
+    def bucle_principal(self):
+        print("soy records")
+
--- arkanoid/game.py
+++ arkanoid/game.py
@@ -1,14 +1,22 @@
 import pygame as pg
 from arkanoid import ALTO, ANCHO
-from arkanoid.escenas import Portada
+from arkanoid.escenas import Portada, Partida, Records
 
 pg.init()
 
 class Game():
     def __init__(self):
         pantalla = pg.display.set_mode((ANCHO, ALTO))
-        self.escenas = [Portada(pantalla)]
+        self.escenas = [Portada(pantalla), Partida(pantalla), Records(pantalla)]
 
     def launch(self):
-        self.escenas[0].bucle_principal()
-        pg.quit()
+        i = 0
+
+        while True:
+            swQuit = self.escenas[i].bucle_principal()
+            i += 1
+            if i == len(self.escenas):
+                i = 0
+
+            # fórmula matemática equivalente al if de arriba
+            # i = (i + 1) % len(self.escenas)                
